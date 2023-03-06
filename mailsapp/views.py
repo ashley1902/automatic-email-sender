90a7088557ca7a0a11f5680911c378ec766c5c38
ashley <127113536+ashley1902@users.noreply.github.com>
2023-03-06 19:41:09 +0530

    Add files via upload

---
+++ mailsapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+
+def Index(request):
+    if request.method == "POST":
+        sub = request.POST.get('subject')
+        msg = request.POST.get('message')
+        email = request.POST.get('email')
+        print(sub,msg,email)
+        send_mail(  
+            sub,msg,'ashwinthandu03@gmail.com',
+            [email]
+        )
+        return HttpResponse('email send that !')
+    return render(request, 'mailsapp/form.html')
